97903fe73a78f7dc5f1efa5263e6a54da5cf4825
Laurent <laurent.marquet@laposte.net>
2024-01-16 13:18:33 +0100

    - Corrected structure (16/01/2024)

--- src/c975LContactFormBundle.php
+++ src/c975LContactFormBundle.php
@@ -18,7 +18,6 @@ class c975LContactFormBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
     {
-        // load an XML, PHP or Yaml file
-        $containerConfigurator->import('config/services.yml');
+        $containerConfigurator->import('../config/services.yml');
     }
 }
